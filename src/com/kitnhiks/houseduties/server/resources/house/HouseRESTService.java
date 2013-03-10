6d1665f1f4adf89de6f1cdad099998e13e0691e5
Kit n Hiks <kitnhiks@gmail.com>
2013-03-10 21:03:12 +0100

    Some log handling on House ressource

--- src/com/kitnhiks/houseduties/server/resources/house/HouseRESTService.java
+++ src/com/kitnhiks/houseduties/server/resources/house/HouseRESTService.java
@@ -7,7 +7,6 @@ import static com.kitnhiks.houseduties.server.utils.AuthTokenizer.generateToken;
 
 import java.util.List;
 
-import javax.jdo.FetchGroup;
 import javax.jdo.PersistenceManager;
 import javax.jdo.Query;
 import javax.jdo.Transaction;
@@ -19,10 +18,9 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
-import org.datanucleus.FetchPlan;
+import org.apache.log4j.Logger;
 
 import com.kitnhiks.houseduties.server.model.House;
 import com.sun.jersey.spi.resource.Singleton;
@@ -30,6 +28,7 @@ import com.sun.jersey.spi.resource.Singleton;
 @Singleton
 @Path("/house")
 public class HouseRESTService {
+	Logger  logger = Logger.getLogger("HouseRESTService");
 
 	@POST
 	@Consumes("application/json")
@@ -40,10 +39,15 @@ public class HouseRESTService {
 	 * @return house id and token
 	 */
 	public Response createAccount(House house){
-		PersistenceManager pm = pmfInstance.getPersistenceManager();
-		pm.makePersistent(house);
-		
-		return Response.status(200).header(AUTH_KEY_HEADER, generateToken(house.getName(), house.getPassword())).entity("{\"id\":\""+house.getId()+"\"}").build();
+		try{
+			PersistenceManager pm = pmfInstance.getPersistenceManager();
+			pm.makePersistent(house);
+
+			String returnEntity = "{\"id\":\""+house.getId()+"\"}";
+			return Response.status(200).header(AUTH_KEY_HEADER, generateToken(house.getName(), house.getPassword())).entity(returnEntity).build();
+		}catch (Exception e){
+			return serverErrorResponse("creating the house", e);
+		}
 	}
 	
 	@Path("login")
@@ -57,7 +61,8 @@ public class HouseRESTService {
 	 */
 	public Response loginAccount(House house){
 
-		PersistenceManager pm = pmfInstance.getPersistenceManager();
+		try{
+			PersistenceManager pm = pmfInstance.getPersistenceManager();
 			Query query = pm.newQuery(House.class, "name == \""+house.getName()+"\" && password == \""+house.getPassword()+"\"");
 			@SuppressWarnings("unchecked")
 			List<House> houseResults = (List<House>) query.execute();
@@ -68,8 +73,11 @@ public class HouseRESTService {
 			}else if (nbResults==0){
 				return Response.status(404).build();
 			}else{
-				return Response.status(500).entity("Corrupted Data...").build();
+				throw new RuntimeException ("Corrupted data...");
 			}
+		}catch (Exception e){
+			return serverErrorResponse("logging to the house", e);
+		}
 	}
 
 	@Path("{id}")
@@ -79,15 +87,19 @@ public class HouseRESTService {
 	 * @return the house
 	 */
 	public Response fetchHouse(@PathParam("id") Long id) {
-		PersistenceManager pm = pmfInstance.getPersistenceManager();
-		House house;
 		try{
-			house = pm.detachCopy(pm.getObjectById(House.class, id));
+			PersistenceManager pm = pmfInstance.getPersistenceManager();
+			House house;
+			try{
+				house = pm.detachCopy(pm.getObjectById(House.class, id));
 			
-		}catch(Exception e){
-			return Response.status(404).build();
+			}catch(Exception e){
+				return Response.status(404).build();
+			}
+			return Response.status(200).entity(house).build();
+		}catch (Exception e){
+			return serverErrorResponse("retrieving the house", e);
 		}
-		return Response.status(200).entity(house).build();
 	}
 
 	@PUT
@@ -98,24 +110,28 @@ public class HouseRESTService {
 	 * @param house
 	 */
 	public Response updateHouse (House house, @PathParam("id") Long id){
-		PersistenceManager pm = pmfInstance.getPersistenceManager();
-		Transaction tx = pm.currentTransaction();
-		try {
-			tx.begin();
-			House houseToUpdate = pm.getObjectById(House.class, id);
-			houseToUpdate.update(house);
-			pm.makePersistent(houseToUpdate);
-			tx.commit();
-			pm.close();
-		}catch(Exception e){
-			return Response.status(500).build();
-		}finally {
-			if (tx.isActive()) {
-				tx.rollback();
+		try{
+			PersistenceManager pm = pmfInstance.getPersistenceManager();
+			Transaction tx = pm.currentTransaction();
+			try {
+				tx.begin();
+				House houseToUpdate = pm.getObjectById(House.class, id);
+				houseToUpdate.update(house);
+				pm.makePersistent(houseToUpdate);
+				tx.commit();
+				pm.close();
+			}catch(Exception e){
+				return Response.status(500).build();
+			}finally {
+				if (tx.isActive()) {
+					tx.rollback();
+				}
+				pm.close();
 			}
-			pm.close();
+			return Response.ok().build();
+		}catch (Exception e){
+			return serverErrorResponse("updating the house", e);
 		}
-		return Response.ok().build();
 	}
 
 	@DELETE
@@ -126,14 +142,23 @@ public class HouseRESTService {
 	 * @param id
 	 */
 	public Response deleteHouse(@PathParam("id") Long id) {
-		PersistenceManager pm = pmfInstance.getPersistenceManager();
-		House houseToDelete;
 		try{
-			houseToDelete = pm.getObjectById(House.class, id);
-		}catch(Exception e){
-			return Response.status(404).build();
+			PersistenceManager pm = pmfInstance.getPersistenceManager();
+			House houseToDelete;
+			try{
+				houseToDelete = pm.getObjectById(House.class, id);
+			}catch(Exception e){
+				return Response.status(404).build();
+			}
+			pm.deletePersistent(houseToDelete);
+			return Response.ok().build();
+		}catch (Exception e){
+			return serverErrorResponse("deleting the house", e);
 		}
-		pm.deletePersistent(houseToDelete);
-		return Response.ok().build();
+	}
+	
+	private Response serverErrorResponse(String whileMessage, Exception e){
+		logger.error("Problem while "+whileMessage + " : " + e.getMessage());
+		return Response.status(500).entity("There has been a problem while "+whileMessage+". Please try again later.").build();
 	}
 }
